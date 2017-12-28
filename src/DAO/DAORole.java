fe292b323f1261af8f69cdb5369cbaeb70a0f240
mk <mkoraichi@outlook.com>
2017-12-28 20:54:54 +0000

    GRestaurant Project

---
+++ src/DAO/DAORole.java
@@ -0,0 +1,111 @@
+package DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import  Classes.Role;
+import Classes.User;
+import  DBLinking.DAO;
+
+/**
+ *
+ * @author mk
+ */
+public class DAORole extends DAO implements IDAORole{
+    private String id = "id";
+    private String name = "name";
+    private DAOUser daoUser=new DAOUser();
+    public DAORole() {
+        super();
+    }
+
+    @Override
+    public int executeQuery(String query, Role o) {
+        statement = createStatement(query);
+        try {
+            statement.setString(1, o.getName());
+            statement.setInt(1, o.getId());
+            return statement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAORole.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Role> executeToArray() {
+        try {
+            ResultSet rs = statement.executeQuery();
+            ArrayList<Role> list = new ArrayList<Role>();
+            while (rs.next()) {
+                list.add(ResultSetToObject(rs));
+            }
+            return list;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAORole.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public int create(Role o) {
+        return executeQuery("INSERT INTO role(name,id) Values(?,?);",o);
+    }
+
+    @Override
+    public int update(Role o) {
+       return executeQuery("UPDATE role SET name=? WHERE id=?;",o);
+    }
+
+    @Override
+    public int delete(int id) {
+        statement = createStatement("DELETE FROM role WHERE id=?;");
+        try {
+            statement.setInt(3, id);
+            return statement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAORole.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    @Override
+    public Role find(int id) {
+        try {
+            statement = createStatement("SELECT * FROM role WHERE id=?;");
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return ResultSetToObject(rs);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAORole.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Role> getAll() {
+            statement = createStatement("SELECT * FROM role;");
+            return executeToArray();
+    }
+
+    @Override
+    public Role ResultSetToObject(ResultSet rs) {
+        try {
+            Role o = new Role(rs.getInt(id), rs.getString(name));
+            o.setUsers(getUsers(o));
+            return o;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAORole.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<User> getUsers(Role role) {
+        return daoUser.findByRole(role);
+    }
+}
